349829022fa14283f21cdd6386cd162a550e7e73
Norm Barnard <norm@normbarnard.com>
2025-12-01 19:36:56 -0500

    added custom formatter

--- Sources/IPAddressLookup/AmazonClient.swift
+++ Sources/IPAddressLookup/AmazonClient.swift
@@ -5,17 +5,15 @@ public enum LookupError: Error {
     case network(Error)
 }
 
-public final class AmazonClient {
+final public class AmazonClient {
     private let url = URL(string: "https://checkip.amazonaws.com")!
 
-    public init() {}
+    public init() { }
 
     public func fetchIPAddress() async throws -> String {
         do {
             let (data, _) = try await URLSession.shared.data(from: url)
-            if let ipAddress = String(data: data, encoding: .utf8)?.trimmingCharacters(
-                in: .whitespacesAndNewlines)
-            {
+            if let ipAddress = String(data: data, encoding: .utf8)?.trimmingCharacters(in: .whitespacesAndNewlines) {
                 return ipAddress
             }
             throw LookupError.badIPAddress(data)
